81e3620c5681ae823ab9b6f18ca4ca9b0dc9507c
Alexander <bergersen.alexander@gmail.com>
2022-07-17 21:49:44 +0200

    initial commit

---
+++ NBAPrediction/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Spark.Sql;
+
+namespace NBAPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spark = SparkSession.Builder().GetOrCreate();
+            var path = "datasets/Advanced.csv";
+            #if DEBUG
+                path = "/workspace/NBAPrediction/" + path;
+            #endif
+            var df = spark.Read()
+                .Format("csv")
+                .Option("sep", ",")
+                .Option("header", true)
+                .Option("inferSchema", true)
+                .Load(path);
+            df.Show(numRows: 20);
+        }
+    }
+}
---
+++ docker-setup/build.sh
@@ -0,0 +1,104 @@
+#!/usr/bin/env bash
+
+# Create different versions of the .NET for Apache Spark runtime docker image
+# based on the Apach Spark and .NET for Apache Spark version.
+
+set -o errexit # abort on nonzero exit status
+set -o nounset # abort on unbound variable
+set -o pipefail # dont hide errors within pipes
+
+readonly dotnet_core_version=3.1
+readonly dotnet_spark_version=2.1.1
+readonly apache_spark_version=3.2.1
+readonly apache_spark_short_version="${apache_spark_version:0:3}"
+readonly hadoop_short_version=2.7
+readonly scala_version=2.11
+proxy=""
+
+main() {
+    echo "Building .NET for Apache Spark ${dotnet_spark_version} runtime image with Apache Spark ${apache_spark_version}"
+
+    build_dotnet_sdk
+    build_dotnet_spark_base_runtime
+    build_dotnet_spark_runtime
+
+    trap finish EXIT ERR
+
+    exit 0
+}
+
+#######################################
+# Runs the docker build command with the related build arguments
+# Arguments:
+#   The image name (incl. tag)
+# Result:
+#   A local docker image with the specified name
+#######################################
+build_image() {
+    local image_name="${1}"
+    local build_args="--build-arg DOTNET_CORE_VERSION=${dotnet_core_version}
+        --build-arg DOTNET_SPARK_VERSION=${dotnet_spark_version}
+        --build-arg SPARK_VERSION=${apache_spark_version}
+        --build-arg HADOOP_VERSION=${hadoop_short_version}"
+    local cmd="docker build ${build_args} -t ${image_name} ."
+
+    if [ -n "${proxy}" ]
+    then
+        build_args+=" --build-arg HTTP_PROXY=${proxy} --build-arg HTTPS_PROXY=${proxy}"
+    fi
+
+    echo "Building ${image_name}"
+
+    ${cmd}
+}
+
+#######################################
+# Use the Dockerfile in the sub-folder dotnet-sdk to build the image of the first stage
+# Result:
+#   A dotnet-sdk docker image tagged with the .NET core version
+#######################################
+build_dotnet_sdk() {
+    local image_name="dotnet-sdk:${dotnet_core_version}"
+
+    cd dotnet-sdk
+    build_image "${image_name}"
+    cd ~-
+}
+
+#######################################
+# Use the Dockerfile in the sub-folder dotnet-spark-base to build the image of the second stage
+# The image contains the specified .NET for Apache Spark version plus the HelloSpark example
+#   for the correct TargetFramework and Microsoft.Spark package version
+# Result:
+#   A dotnet-spark-base-runtime docker image tagged with the .NET for Apache Spark version
+#######################################
+build_dotnet_spark_base_runtime() {
+    local image_name="dotnet-spark-base-runtime:${dotnet_spark_version}"
+
+    cd dotnet-spark-base
+    build_image "${image_name}"
+    cd ~-
+}
+
+#######################################
+# Use the Dockerfile in the sub-folder dotnet-spark to build the image of the last stage
+# The image contains the specified Apache Spark version
+# Result:
+#   A dotnet-spark docker image tagged with the .NET for Apache Spark version and the Apache Spark version.
+#######################################
+build_dotnet_spark_runtime() {
+    local image_name="dotnet-spark:${dotnet_spark_version}-${apache_spark_version}"
+
+    cd dotnet-spark
+    build_image "${image_name}"
+    cd ~-
+}
+
+
+finish()
+{
+    result=$?
+    exit ${result}
+}
+
+main "${@}"
