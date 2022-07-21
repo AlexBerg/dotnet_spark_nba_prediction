539083a297f428ed56f90b7a8f3adc00c01d8b5d
Alexander <bergersen.alexander@gmail.com>
2022-07-21 11:48:09 +0200

    team and team stats table modeling should be done

--- NBAPrediction/Program.cs
+++ NBAPrediction/Program.cs
@@ -12,28 +12,10 @@ namespace NBAPrediction
         {
             var helper = new HelperService();
             var spark = helper.GetSparkSession();
-            var path = "/workspace/NBAPrediction/datasets/Advanced.csv";
-            var df = helper.LoadFromCsv(spark, path);
-            df.Show(numRows: 20);
 
-            var db = spark.Catalog.CurrentDatabase();
-            var dbs = spark.Catalog.ListDatabases();
+            var dataCleaner = new DataCleanerService(helper);
 
-            dbs.Show();
-
-            if(!spark.Catalog.TableExists("advanced_stats"))
-                df.Write().Format("delta").Mode("overwrite")
-                    .Option("overwriteSchema", true)
-                    .SaveAsTable("advanced_stats");
-            else 
-            {
-                df = spark.Read().Table("advanced_stats");
-                df.Show(numRows: 20, truncate: 0);
-            }
-
-            var tables = spark.Catalog.ListTables();
-
-            tables.Show();
+            dataCleaner.CreateNBADeltaTables(spark);
         }
     }
 }
--- NBAPrediction/Services/DataCleanerService.cs
+++ NBAPrediction/Services/DataCleanerService.cs
@@ -10,7 +10,10 @@ namespace NBAPrediction.Services
     internal class DataCleanerService : IDataCleanerService
     {
         private readonly IHelperService _helperService;
-        
+
+        public DataCleanerService(IHelperService helperService) {
+            _helperService = helperService;
+        }
 
         public void CreateNBADeltaTables(SparkSession spark)
         {
@@ -23,31 +26,34 @@ namespace NBAPrediction.Services
                 "Team Summaries"
             };
 
-            DataFrame advancedStats = _helperService.LoadFromCsv(spark, "Advanced.csv")
-                .Select("player_id", "season", "tm", "g", "mp", "per", "ts_percent", "x3p_ar", "f_tr", "orb_percent", "drb_percent", "trb_percent",
-                "ast_percent", "stl_percent", "blk_percent", "tov_percent", "usg_percent", "ows", "dws", "ws", "ws_48", "obpm", "dbpm", "bpm", "vorp")
-                .Filter("tm != TOT")
-                .Na().Replace("*", new Dictionary<string, string>() { { "NA", null } });
+            // DataFrame advancedStats = _helperService.LoadFromCsv(spark, "Advanced.csv")
+            //     .Select("player_id", "season", "tm", "g", "mp", "per", "ts_percent", "x3p_ar", "f_tr", "orb_percent", "drb_percent", "trb_percent",
+            //     "ast_percent", "stl_percent", "blk_percent", "tov_percent", "usg_percent", "ows", "dws", "ws", "ws_48", "obpm", "dbpm", "bpm", "vorp")
+            //     .Filter("tm != TOT")
+            //     .Na().Replace("*", new Dictionary<string, string>() { { "NA", null } });
 
         }
 
         private void CreateTeamTables(SparkSession spark)
         {
-            DataFrame teamSummaries = _helperService.LoadFromCsv(spark, "Team Summaries.csv")
-                .Filter("team != 'League Average'");
+            DataFrame teamSummaries = _helperService.LoadFromCsv(spark, "/workspace/NBAPrediction/datasets/Team Summaries.csv")
+                .Filter("team != 'League Average' AND abbreviation != 'NA'");
 
             DataFrame teams = teamSummaries
                 .Select(F.Col("team").As("TeamName"), 
                     F.Col("abbreviation").As("TeamNameShort"), 
                     F.Col("lg").As("League"))
                 .Distinct()
-                .WithColumn("TeamId", F.MonotonicallyIncreasingId().Cast("string"));
+                .WithColumn("TeamId", 
+                    F.Concat(F.Hex(F.Col("League")), F.Hex(F.Col("TeamNameShort"))));
 
             teams.Write().Format("delta").Mode(SaveMode.Overwrite).SaveAsTable("Teams");
 
             DataFrame teamSeasonStats = teamSummaries
-                .Select(F.Col("season").As("Season"),
-                    F.Col("abbreviation"),
+                .Join(teams, teamSummaries["abbreviation"] == teams["TeamNameShort"] 
+                    & teamSummaries["lg"] == teams["League"], "inner")
+                .Select(F.Col("TeamId"), 
+                    F.Col("season").As("Season"),
                     F.Col("playoffs").As("MadePlayoffs").Cast("boolean"),
                     F.Col("age").As("AverageAge"),
                     F.Col("w").As("Wins").Cast("short"),
@@ -55,7 +61,26 @@ namespace NBAPrediction.Services
                     F.Col("pw").As("PredictedWins").Cast("short"),
                     F.Col("pl").As("PredictedLosses").Cast("short"),
                     F.Col("mov").As("AverageMarginOfVictory"),
-                    F.Col("sos").As("StrengthOfSchedule"),)
+                    F.Col("sos").As("StrengthOfSchedule"),
+                    F.Col("srs").As("SimpleRating"),
+                    F.Col("o_rtg").As("OffensiveRating"),
+                    F.Col("d_rtg").As("DefensiveRating"),
+                    F.Col("n_rtg").As("NetRating"),
+                    F.Col("pace").As("Pace"),
+                    F.Col("f_tr").As("FreeThrowRate"),
+                    F.Col("x3p_ar").As("3PointAttemptRate"),
+                    F.Col("ts_percent").As("TrueShootingPercentage"),
+                    F.Col("e_fg_percent").As("EffectiveFieldGoalPercentage"),
+                    F.Col("tov_percent").As("TurnoverPercentage"),
+                    F.Col("orb_percent").As("OffensiveReboundPercentage"),
+                    F.Col("ft_fga").As("FreeThrowFactor"),
+                    F.Col("opp_e_fg_percent").As("OpponentEFGPercentage"),
+                    F.Col("opp_tov_percent").As("OpponentTOVPercentage"),
+                    F.Col("opp_ft_fga").As("OpponentFreeThrowFactor"));
+
+            teamSeasonStats = CastColumnsToFloat(teamSeasonStats);
+
+            teamSeasonStats.Write().Format("delta").Mode(SaveMode.Overwrite).SaveAsTable("TeamSeasonStats");
         }
 
         private DataFrame CastColumnsToFloat(DataFrame dataFrame) 
