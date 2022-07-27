27750c412a1f12ef6a2377430f5e72d11292e3a5
Alexander <bergersen.alexander@gmail.com>
2022-07-27 11:04:36 +0200

    Solved some data modeling bugs as well as bugs in the SQL script to grab data for learning

--- NBAPrediction/Program.cs
+++ NBAPrediction/Program.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Spark.Sql;
 using NBAPrediction.Services;
-using Microsoft.Spark.Extensions.Delta.Tables;
-using Microsoft.Spark.Sql.Catalog;
-using System;
+
 
 namespace NBAPrediction
 {
@@ -15,7 +13,7 @@ namespace NBAPrediction
 
             var training = new TrainingService(helper);
 
-            training.TrainMVPPredicitionModel(spark);
+            training.TrainAndEvaluateMVPPredicitionModel(spark);
         }
     }
 }
